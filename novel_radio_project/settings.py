926201df9611d732fe8d8a27fb7f49a74834a595
gasaki8881 <hituzikai2@docomo.ne.jp>
2020-01-18 20:14:28 +0900

    'aeervsre'

--- novel_radio_project/settings.py
+++ novel_radio_project/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 EMAIL_HOST = 'smtp.gmail.com'
